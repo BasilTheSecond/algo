b924df8ca90c4b277808b4d3f5fdec415edc316f
Dragan Ostojic <dragan.ostojic@outlook.com>
2016-12-05 02:30:27 -0500

    saved

--- dp/BellmanFord/graph2/BellmanFord/BellmanFord.cpp
+++ dp/BellmanFord/graph2/BellmanFord/BellmanFord.cpp
@@ -3,12 +3,18 @@
 
 #include "stdafx.h"
 #include <vector>
+#include <map>
 #include <iostream>
 #include <algorithm>
 
 void iterativeBellmanFord();
 void recursiveBellmanFord();
 int deltaSS(int k);
+int deltaSA(int k);
+int deltaSB(int k);
+int deltaSC(int k);
+int deltaSD(int k);
+int deltaSE(int k);
 const int inf = std::numeric_limits<int>::max();
 const int wSA = 10;
 const int wSE = 8;
@@ -24,10 +30,16 @@ std::vector<int> dSB;
 std::vector<int> dSC;
 std::vector<int> dSD;
 std::vector<int> dSE;
+std::map<int, int> memdSA;
+std::map<int, int> memdSB;
+std::map<int, int> memdSC;
+std::map<int, int> memdSD;
+std::map<int, int> memdSE;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	iterativeBellmanFord();
+	recursiveBellmanFord();
 	getchar();
 	return 0;
 }
@@ -64,10 +76,63 @@ void iterativeBellmanFord()
 void recursiveBellmanFord()
 {
 	std::cout << "recursiveBellmanFord" << std::endl;
-
+	std::cout << "delta(S,S)=" << deltaSS(5) << std::endl;
+	std::cout << "delta(S,A)=" << deltaSA(5) << std::endl;
+	std::cout << "delta(S,B)=" << deltaSB(5) << std::endl;
+	std::cout << "delta(S,C)=" << deltaSC(5) << std::endl;
+	std::cout << "delta(S,D)=" << deltaSD(5) << std::endl;
+	std::cout << "delta(S,E)=" << deltaSE(5) << std::endl;
 }
 
 int deltaSS(int k)
 {
 	return 0;
+}
+
+int deltaSA(int k)
+{
+	if (memdSA.find(k) == memdSA.end())
+	{
+		memdSA[k] = std::min(deltaSD(k - 1) + wDA,
+								std::min(deltaSB(k - 1) + wBA,
+													deltaSS(k - 1) + wSA));
+	}
+	return memdSA[k];
+}
+
+int deltaSB(int k)
+{
+	if (memdSB.find(k) == memdSB.end())
+	{
+		memdSB[k] = deltaSC(k - 1) + wCB;
+	}
+	return memdSB[k];
+}
+
+int deltaSC(int k)
+{
+	if (memdSC.find(k) == memdSC.end())
+	{
+		memdSC[k] = std::min(deltaSD(k - 1) + wDC,
+													deltaSA(k - 1) + wAC);
+	}
+	return memdSC[k];
+}
+
+int deltaSD(int k)
+{
+	if (memdSD.find(k) == memdSD.end())
+	{
+		memdSD[k] = deltaSE(k - 1) + wED;
+	}
+	return memdSD[k];
+}
+
+int deltaSE(int k)
+{
+	if (memdSE.find(k) == memdSE.end())
+	{
+		memdSE[k] = deltaSS(k - 1) + wSE;
+	}
+	return memdSE[k];
 }
