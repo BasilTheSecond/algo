091f1800c0c3978a279385369e8caf808db64ef6
Dragan Ostojic <dragan.ostojic@outlook.com>
2016-12-06 17:10:10 -0500

    saved

--- dp/EditDistance/EditDistance/Test.cpp
+++ dp/EditDistance/EditDistance/Test.cpp
@@ -1,10 +1,26 @@
 #include "stdafx.h"
 #include <iostream>
 
+class EditDistance
+{
+public:
+	EditDistance();
+	~EditDistance();
+};
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	std::cout << "Press any key to exit" << std::endl;
+	EditDistance editDistance;
 	getchar();
 	return 0;
+}
+
+EditDistance::EditDistance()
+{
+}
+
+EditDistance::~EditDistance()
+{
 }
 
