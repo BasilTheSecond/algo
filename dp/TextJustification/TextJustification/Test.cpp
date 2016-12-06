193bdc95ec7bdd3b14e0cc4e187eff4e9bf81150
Dragan Ostojic <dragan.ostojic@outlook.com>
2016-12-05 19:43:14 -0500

    saved

--- dp/TextJustification/TextJustification/Test.cpp
+++ dp/TextJustification/TextJustification/Test.cpp
@@ -4,13 +4,16 @@
 #include <vector>
 #include <sstream>
 #include <algorithm>
+#include <fstream>
 
 class TextJustification
 {
 public:
 	TextJustification(const std::string& text, int lineWidth);
+	TextJustification(std::ifstream& ifile, int lineWidth);
 	~TextJustification();
 	void mPrintWords();
+	void mPrintJustified(std::ofstream& ofile);
 	
 private:
 	void mSplitText(const std::string& text);
@@ -21,6 +24,7 @@ private:
 	std::vector<std::string> m_words;
 	const int m_lineWidth;
 	std::vector<double> m_costs;
+	std::vector<int> m_parent;
 
 private:
 	static const double k_inf;
@@ -30,9 +34,12 @@ const double TextJustification::k_inf = std::numeric_limits<double>::max();
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	std::string text("It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).");
+	std::ifstream ifile("unjustified.txt");
 	int lineWidth = 80;
-	TextJustification textJustification(text, lineWidth);
+	TextJustification textJustification1(ifile, lineWidth);
+	textJustification1.mPrintWords();
+	std::ofstream ofile("justified.txt");
+	textJustification1.mPrintJustified(ofile);
 	getchar();
 	return 0;
 }
@@ -41,7 +48,15 @@ TextJustification::TextJustification(const std::string& text, int lineWidth) :
 	m_lineWidth(lineWidth)
 {
 	mSplitText(text);
-	mPrintWords();
+	mCalculateCosts();
+}
+
+TextJustification::TextJustification(std::ifstream& ifile, int lineWidth) :
+	m_lineWidth(lineWidth)
+{
+	std::stringstream sstream;
+	sstream << ifile.rdbuf();
+	mSplitText(sstream.str());
 	mCalculateCosts();
 }
 
@@ -73,12 +88,26 @@ void TextJustification::mCalculateCosts()
 {
 	m_costs.clear();
 	int n = m_words.size();
-	m_costs.resize(n + 1, k_inf);
+	m_costs.resize(n + 1);
+	m_parent.resize(m_words.size());
 	m_costs[n] = 0;
-	m_costs[n - 1] = std::min(mCost(n - 1, n) + m_costs[n], m_costs[n - 1]);
-	//
-	m_costs[n - 2] = std::min(mCost(n - 2, n - 1) + m_costs[n - 1], m_costs[n - 2]);
-	m_costs[n - 2] = std::min(mCost(n - 2, n) + m_costs[n], m_costs[n - 2]);
+	// subproblem iteration formula:
+	// k = n - 1, n - 2, ... 0 (start from the end)
+	// costs[k] = min {cost(k, i) + costs[i]}
+	//					k < i < n+1
+	for (int k = n - 1; k >= 0; k--)
+	{
+		m_costs[k] = k_inf;
+		for (int i = k + 1; i < n + 1; i++)
+		{
+			double cost = mCost(k, i) + m_costs[i];
+			if (cost < k_inf)
+			{
+				m_parent[k] = i;
+			}
+			m_costs[k] = std::min(mCost(k, i) + m_costs[i], m_costs[k]);
+		}
+	}
 }
 
 double TextJustification::mCost(int i, int j)
@@ -91,4 +120,19 @@ double TextJustification::mCost(int i, int j)
 	totalWidth += (j - i - 1); // space
 	int extraSpace = m_lineWidth - totalWidth;
 	return extraSpace < 0 ? k_inf : std::pow(extraSpace, 3);
+}
+
+void TextJustification::mPrintJustified(std::ofstream& ofile)
+{
+	for (int i = 0; true; i = m_parent[i])
+	{
+		for (int j = i; j < m_parent[i]; j++)
+		{
+			ofile << m_words[j] << ((j < m_parent[i] - 1) ? " " : "\n");
+		}
+		if (m_parent[i] == m_words.size())
+		{
+			break;
+		}
+	}
 }
