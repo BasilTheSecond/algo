9f62f542a73d3e8c26e1fc54a34e5ae89ecb02ad
Dragan Ostojic <dragan.ostojic@outlook.com>
2016-12-06 18:41:00 -0500

    saved

--- dp/EditDistance/EditDistance/Test.cpp
+++ dp/EditDistance/EditDistance/Test.cpp
@@ -3,6 +3,61 @@
 #include <string>
 #include <vector>
 
+// Explanations of insertion, deletion and substitution steps:
+// Elements in the edit step table represent x in the various stages of transformation
+// towards y. In general:
+// x(i, j)	= xorig[0:i-1]	|| x[i] || y[j+1:]
+// y				= y[0:j-1]			|| y[j] || y[j+1:]
+// NOTE: x(i, j) has prefix 0:i as original x and the rest is suffix y[j+1:]
+// If y[j+1:] suffix is aligned with the same suffix in y, then x[i] and y[j] are aligned
+// and x string manipulation happens at this point.
+// After one of the steps, x(i, j) goes to one of x(i-1, j-1), x(i-1, j) or x(i, j-1)
+// 
+// 1) Insertion step
+// =================
+// insert y[j] after x[i]
+// x(i, j) -> x(i, j-1)
+// Exlanation of the step:
+// Before insertion:
+// x(i, j)	= xorig[0:i-1]	|| x[i] || y[j+1:]
+// y				= y[0:j-1]			|| y[j] || y[j+1:]
+// After insertion of y[j] after x[i]
+// x = xorig[0:i-1]	|| x[i]		|| y[j] || y[j+1:]
+// y = y[0:j-2]			|| y[j-1]	|| y[j] || y[j+1:]
+// Therfore:
+// x(i, j-1)	= xorig[0:i-1]	|| x[i]		|| y[j:]
+// y					= y[0:j-2]			|| y[j-1] || y[j:]
+// 2) Deletion step
+// =================
+// delete x[i]
+// x(i, j) -> x(i-1, j)
+// Exlanation of the step:
+// Before deletion:
+// x(i, j)	= xorig[0:i-1]	|| x[i] || y[j+1:]
+// y				= y[0:j-1]			|| y[j] || y[j+1:]
+// After deleting of x[i]
+// x = xorig[0:i-2]	|| x[i-1] || y[j+1:]
+// y = y[0:j-1]			|| y[j]		|| y[j+1:]
+// Therfore:
+// x(i-1, j)	= xorig[0:i-2]	|| x[i-1]	|| y[j+1:]
+// y					= y[0:j-1]			|| y[j]		|| y[j+1:]
+// 3) Substitution step
+// =================
+// substitute x[i] with y[j]
+// x(i, j) -> x(i-1, j-1)
+// Exlanation of the step:
+// Before deletion:
+// x(i, j)	= xorig[0:i-1]	|| x[i] || y[j+1:]
+// y				= y[0:j-1]			|| y[j] || y[j+1:]
+// After substituting x[i] with y[j]
+// x = xorig[0:i-1]	|| y[j] || y[j+1:]
+// y = y[0:j-1]			|| y[j]	|| y[j+1:]
+// Therfore:
+// x(i-1, j-1)	= xorig[0:i-2]	|| x[i-1]	|| y[j] || y[j+1:]
+// y					= y[0:j-2]				|| y[j-1]	|| y[j]	|| y[j+1:]
+// x(i-1, j-1)	= xorig[0:i-2]	|| x[i-1]	|| y[j:]
+// y					= y[0:j-2]				|| y[j-1]	|| y[j:]
+
 class EditDistance
 {
 public:
@@ -38,6 +93,14 @@ EditDistance::EditDistance(std::string& x, std::string& y) :
 		m_table[i].resize(m_y.size());
 	}
 	m_table[0][0] = mSubstitutionCost(0, 0);
+	for (size_t i = 1; i < m_x.size(); i++)
+	{
+		m_table[i][0] = m_table[i - 1][0] + mDeletionCost(i - 1, 0);
+	}
+	for (size_t j = 1; j < m_y.size(); j++)
+	{
+		m_table[0][j] = m_table[0][j - 1] + mInsertionCost(0, j - 1);
+	}
 }
 
 EditDistance::~EditDistance()
