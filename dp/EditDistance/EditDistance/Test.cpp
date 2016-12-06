5ce8eb386eaea372642044d23117ae232df2abe4
Dragan Ostojic <dragan.ostojic@outlook.com>
2016-12-06 17:35:58 -0500

    saved

--- dp/EditDistance/EditDistance/Test.cpp
+++ dp/EditDistance/EditDistance/Test.cpp
@@ -1,26 +1,59 @@
 #include "stdafx.h"
 #include <iostream>
+#include <string>
+#include <vector>
 
 class EditDistance
 {
 public:
-	EditDistance();
+	EditDistance(std::string& x, std::string& y);
 	~EditDistance();
+	double mInsertionCost(int i, int j);
+	double mDeletionCost(int i, int j);
+	double mSubstitutionCost(int i, int j);
+
+private:
+	std::string m_x;
+	std::string m_y;
+	std::vector<std::vector<int>> m_table;
 };
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	std::string x("#EXECUTION");
+	std::string y("#INTENTION");
+	EditDistance editDistance(x, y);
 	std::cout << "Press any key to exit" << std::endl;
-	EditDistance editDistance;
 	getchar();
 	return 0;
 }
 
-EditDistance::EditDistance()
+EditDistance::EditDistance(std::string& x, std::string& y) :
+	m_x(x),
+	m_y(y)
 {
+	m_table.resize(m_x.size());
+	for (size_t i = 0; i < m_table.size(); i++)
+	{
+		m_table[i].resize(m_y.size());
+	}
 }
 
 EditDistance::~EditDistance()
 {
+}
+
+double EditDistance::mInsertionCost(int i, int j)
+{
+	return 1;
+}
+
+double EditDistance::mDeletionCost(int i, int j)
+{
+	return 1;
+}
+double EditDistance::mSubstitutionCost(int i, int j)
+{
+	return (m_x[i] == m_y[j]) ? 0 : 2;
 }
 
