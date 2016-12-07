4aa5409e4fe7a123cb4aea8c6e4b9aca70c55c18
Dragan Ostojic <dragan.ostojic@outlook.com>
2016-12-07 14:57:36 -0500

    saved

--- dp/Knapsack/Knapsack/Test.cpp
+++ dp/Knapsack/Knapsack/Test.cpp
@@ -1,9 +1,24 @@
 #include "stdafx.h"
 #include <iostream>
 
+class KnapSack
+{
+public:
+	KnapSack();
+	~KnapSack();
+};
+
 int _tmain(int argc, _TCHAR* argv[])
 {
+	KnapSack knapSack;
 	std::cout << "Press any key to exit..." << std::endl;
 	return 0;
 }
 
+KnapSack::KnapSack()
+{
+}
+
+KnapSack::~KnapSack()
+{
+}
