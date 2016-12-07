0a360458917686b3970da4edb7c6a7e09c045e15
Dragan Ostojic <dragan.ostojic@outlook.com>
2016-12-07 15:06:13 -0500

    saved

--- dp/Knapsack/Knapsack/Test.cpp
+++ dp/Knapsack/Knapsack/Test.cpp
@@ -1,11 +1,27 @@
 #include "stdafx.h"
 #include <iostream>
+#include <map>
 
 class KnapSack
 {
 public:
+	class Value
+	{
+	public:
+		Value();
+		~Value();
+
+	public:
+		std::pair<int, int> m_parent;
+		std::pair<int, int> m_value;
+	};
+
+public:
 	KnapSack();
 	~KnapSack();
+
+private:
+	std::map<std::pair<int, int>, Value> m_values;
 };
 
 int _tmain(int argc, _TCHAR* argv[])
@@ -20,5 +36,13 @@ KnapSack::KnapSack()
 }
 
 KnapSack::~KnapSack()
+{
+}
+
+KnapSack::Value::Value()
+{
+}
+
+KnapSack::Value::~Value()
 {
 }
