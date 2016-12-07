ce460bd3c0fb36d901b8828c8dfd17ed11721ca3
Dragan Ostojic <dragan.ostojic@outlook.com>
2016-12-07 15:24:15 -0500

    saved

--- dp/Knapsack/Knapsack/Test.cpp
+++ dp/Knapsack/Knapsack/Test.cpp
@@ -31,12 +31,13 @@ public:
 	};
 
 public:
-	KnapSack(std::vector<Item>& items);
+	KnapSack(std::vector<Item>& items, int size);
 	~KnapSack();
 
 private:
 	std::map<std::pair<int, int>, Value> m_values;
 	std::vector<Item>& m_items;
+	int m_size;
 };
 
 int _tmain(int argc, _TCHAR* argv[])
@@ -45,14 +46,15 @@ int _tmain(int argc, _TCHAR* argv[])
 	items.push_back(KnapSack::Item(std::string("gold watch"), 10, 3));
 	items.push_back(KnapSack::Item(std::string("box of matches"), 5, 1));
 	items.push_back(KnapSack::Item(std::string("sandwich"), 4, 4));
-	KnapSack knapSack(items);
+	KnapSack knapSack(items, 5);
 	std::cout << "Press any key to exit..." << std::endl;
 	getchar();
 	return 0;
 }
 
-KnapSack::KnapSack(std::vector<Item>& items) :
-m_items(items)
+KnapSack::KnapSack(std::vector<Item>& items, int size) :
+m_items(items),
+m_size(size)
 {
 }
 
