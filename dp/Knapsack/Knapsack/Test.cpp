22df51780103abcc47e84b76b451bc2230d86fc6
Dragan Ostojic <dragan.ostojic@outlook.com>
2016-12-07 22:19:16 -0500

    saved

--- dp/Knapsack/Knapsack/Test.cpp
+++ dp/Knapsack/Knapsack/Test.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 class KnapSack
 {
@@ -45,7 +46,7 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	std::vector<KnapSack::Item> items;
 	items.push_back(KnapSack::Item(std::string("gold watch"), 10, 3));
-	items.push_back(KnapSack::Item(std::string("box of matches"), 5, 1));
+	items.push_back(KnapSack::Item(std::string("box of matches"), 1, 5));
 	items.push_back(KnapSack::Item(std::string("sandwich"), 4, 4));
 	KnapSack knapSack(items, 5);
 	std::cout << "Press any key to exit..." << std::endl;
@@ -59,7 +60,25 @@ m_size(size)
 {
 	for (int j = 0; j <= m_size; j++)
 	{
-		m_values[std::pair<int, int>(m_items.size() - 1, j)] = Value(std::pair<int, int>(0, 0), j < m_items[m_items.size() - 1].m_size ? 0 : m_items[m_items.size() - 1].m_value);
+		m_values[std::pair<int, int>(m_items.size() - 1, j)] = Value(std::pair<int, int>(-1, -1), j < m_items[m_items.size() - 1].m_size ? 0 : m_items[m_items.size() - 1].m_value);
+	}
+	for (int i = m_items.size() - 2; i >= 0; i--)
+	{
+		for (int j = 0; j <= m_size; j++)
+		{
+			std::vector<Value> values;
+			if (j - m_items[i].m_size >= 0)
+			{
+				values.push_back(Value(std::pair<int, int>(i + 1, j - m_items[i].m_size), m_values[std::pair<int, int>(i + 1, j - m_items[i].m_size)].m_value + m_items[i].m_value));
+			}
+			values.push_back(Value(std::pair<int, int>(i + 1, j), m_values[std::pair<int, int>(i + 1, j)].m_value));
+			std::sort(values.begin(), values.end(), 
+								([](Value& a, Value& b)
+								{
+									return a.m_value >= b.m_value;
+								}));
+			m_values[std::pair<int, int>(i, j)] = values[0];
+		}
 	}
 }
 
