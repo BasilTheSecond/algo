fff11072177c60f50d8115ed06ff13b26f8085fd
Dragan Ostojic <dragan.ostojic@outlook.com>
2016-12-08 16:19:37 -0500

    saved

--- dp/EditDistance/EditDistance/Test.cpp
+++ dp/EditDistance/EditDistance/Test.cpp
@@ -81,12 +81,12 @@ private:
 	virtual double mSubstitutionCost(int i, int j) = 0;
 
 private:
-	class BackTrace
+	class Cost
 	{
 	public:
-		BackTrace();
-		BackTrace(std::pair<int, int>& parent, double cost, std::string& operation);
-		~BackTrace();
+		Cost();
+		Cost(std::pair<int, int>& parent, double cost, std::string& operation);
+		~Cost();
 
 	public:
 		std::pair<int, int> m_parent;
@@ -95,7 +95,7 @@ private:
 	};
 
 private:
-	std::map<std::pair<int, int>, BackTrace> m_table; // table with back-trace
+	std::map<std::pair<int, int>, Cost> m_table; // table with back-trace
 };
 
 class LevenshteinDistance : public EditDistance
@@ -124,26 +124,31 @@ private:
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	std::string x1("#EXECUTION");
-	std::string y1("#INTENTION");
-	LevenshteinDistance levenshteinDistance1(x1, y1);
+	std::string x0("#hello");
+	std::string y0("#soo");
+	LevenshteinDistance levenshteinDistance0(x0, y0);
 	std::cout << "Steps:" << std::endl;
-	levenshteinDistance1.mPrintSteps();
-	std::string x2("#HELLO,WORLD");
-	std::string y2("#BLAH,BLAH");
-	LevenshteinDistance levenshteinDistance2(x2, y2);
-	std::cout << "Steps:" << std::endl;
-	levenshteinDistance2.mPrintSteps();
-	std::string x3("AGGCTATCACCTGACCTCCAGGCCGATGCCC");
-	std::string y3("TAGCTATCACGACCGCGGTCGATTTGCCCGAC");
-	LevenshteinDistance levenshteinDistance3(x3, y3);
-	std::cout << "Steps:" << std::endl;
-	levenshteinDistance3.mPrintSteps();
-	std::string x4("HIEROGLYPHOLOGY");
-	std::string y4("MICHAELANGELO");
-	LongestCommonSubsequence longestCommonSubsequence4(x4, y4);
-	std::cout << "Steps:" << std::endl;
-	longestCommonSubsequence4.mPrintSteps();
+	levenshteinDistance0.mPrintSteps();
+	//std::string x1("#EXECUTION");
+	//std::string y1("#INTENTION");
+	//LevenshteinDistance levenshteinDistance1(x1, y1);
+	//std::cout << "Steps:" << std::endl;
+	//levenshteinDistance1.mPrintSteps();
+	//std::string x2("#HELLO,WORLD");
+	//std::string y2("#BLAH,BLAH");
+	//LevenshteinDistance levenshteinDistance2(x2, y2);
+	//std::cout << "Steps:" << std::endl;
+	//levenshteinDistance2.mPrintSteps();
+	//std::string x3("#AGGCTATCACCTGACCTCCAGGCCGATGCCC");
+	//std::string y3("#TAGCTATCACGACCGCGGTCGATTTGCCCGAC");
+	//LevenshteinDistance levenshteinDistance3(x3, y3);
+	//std::cout << "Steps:" << std::endl;
+	//levenshteinDistance3.mPrintSteps();
+	//std::string x4("#HIEROGLYPHOLOGY");
+	//std::string y4("#MICHAELANGELO");
+	//LongestCommonSubsequence longestCommonSubsequence4(x4, y4);
+	//std::cout << "Steps:" << std::endl;
+	//longestCommonSubsequence4.mPrintSteps();
 	std::cout << "Press any key to exit" << std::endl;
 	getchar();
 	return 0;
@@ -161,38 +166,47 @@ EditDistance::~EditDistance()
 
 void EditDistance::mComputeTable()
 {
-	m_table[std::pair<int, int>(0, 0)] = BackTrace(std::pair<int, int>(-1, -1), mSubstitutionCost(0, 0), std::string("substitute"));
-	for (size_t i = 1; i < m_x.size(); i++)
+	for (int i = 0; i < static_cast<int>(m_x.size()); i++)
 	{
-		m_table[std::pair<int, int>(i, 0)] = BackTrace(std::pair<int, int>(i - 1, 0), m_table[std::pair<int, int>(i - 1, 0)].m_cost + mDeletionCost(i, 0), std::string("delete"));
-	}
-	for (size_t j = 1; j < m_y.size(); j++)
-	{
-		m_table[std::pair<int, int>(0, j)] = BackTrace(std::pair<int, int>(0, j - 1), m_table[std::pair<int, int>(0, j - 1)].m_cost + mInsertionCost(0, j), std::string("insert"));
-	}
-	for (size_t i = 1; i < m_x.size(); i++)
-	{
-		for (size_t j = 1; j < m_y.size(); j++)
+		for (int j = 0; j < static_cast<int>(m_y.size()); j++)
 		{
-			std::vector<BackTrace> costs;
-			costs.push_back(BackTrace(std::pair<int, int>(i - 1, j - 1), m_table[std::pair<int, int>(i - 1, j - 1)].m_cost + mSubstitutionCost(i, j), std::string("substitute")));
-			costs.push_back(BackTrace(std::pair<int, int>(i - 1, j), m_table[std::pair<int, int>(i - 1, j)].m_cost + mDeletionCost(i, j), std::string("delete")));
-			costs.push_back(BackTrace(std::pair<int, int>(i, j - 1), m_table[std::pair<int, int>(i, j - 1)].m_cost + mInsertionCost(i, j), std::string("insert")));
-			m_table[std::pair<int, int>(i, j)] = *std::min_element(costs.begin(),	costs.end(), 
-				[](BackTrace& a, BackTrace& b)
+			std::vector<Cost> costs;
+			Cost minCost;
+			if (i - 1 >= 0 && j - 1 >= 0)
+			{
+				costs.push_back(Cost(std::pair<int, int>(i - 1, j - 1), m_table[std::pair<int, int>(i - 1, j - 1)].m_cost + mSubstitutionCost(i, j), std::string("substitute")));
+				costs.push_back(Cost(std::pair<int, int>(i - 1, j), m_table[std::pair<int, int>(i - 1, j)].m_cost + mDeletionCost(i, j), std::string("delete")));
+				costs.push_back(Cost(std::pair<int, int>(i, j - 1), m_table[std::pair<int, int>(i, j - 1)].m_cost + mInsertionCost(i, j), std::string("insert")));
+				auto lambdaMin = [](Cost& a, Cost& b)
 				{
 					return a.m_cost < b.m_cost;
-				});
+				};
+				std::vector<Cost>::iterator iteratorCost = std::min_element(costs.begin(), costs.end(), lambdaMin);
+				minCost = *iteratorCost;
+			}
+			else if (i == 0 && j - 1 >= 0)
+			{
+				minCost = Cost(std::pair<int, int>(i, j - 1), m_table[std::pair<int, int>(i, j - 1)].m_cost + mInsertionCost(i, j), std::string("insert"));
+			}
+			else if (i - 1 >= 0 && j == 0)
+			{
+				minCost = Cost(std::pair<int, int>(i - 1, j), m_table[std::pair<int, int>(i - 1, j)].m_cost + mDeletionCost(i, j), std::string("delete"));
+			}
+			else // i == 0 && j == 0
+			{
+				minCost = Cost(std::pair<int, int>(-1, -1), 0, std::string(""));
+			}
+			m_table[std::pair<int, int>(i, j)] = minCost;
 		}
 	}
 }
 
 void EditDistance::mPrintSteps()
 {
-	BackTrace root = m_table[std::pair<int, int>(m_x.size() - 1, m_y.size() - 1)];
+	Cost root = m_table[std::pair<int, int>(m_x.size() - 1, m_y.size() - 1)];
 	std::cout << "(" << (m_x.size() - 1) << "," << (m_y.size() - 1) << "): ";
 	std::cout << m_x << "-" << std::endl;
-	for (BackTrace next = root; true; next = m_table[std::pair<int, int>(next.m_parent)])
+	for (Cost next = root; true; next = m_table[std::pair<int, int>(next.m_parent)])
 	{
 		std::cout << "(" << next.m_parent.first << "," << next.m_parent.second << "): ";
 		std::cout << m_x.substr(0, next.m_parent.first + 1) << "-" << m_y.substr(next.m_parent.second + 1) << " " << next.m_operation << "(" << next.m_cost << ")" << std::endl;
@@ -205,18 +219,18 @@ void EditDistance::mPrintSteps()
 	}
 }
 
-EditDistance::BackTrace::BackTrace(std::pair<int, int>& parent, double cost, std::string& operation) :
-	m_parent(parent),
-	m_cost(cost),
-	m_operation(operation)
+EditDistance::Cost::Cost(std::pair<int, int>& parent, double cost, std::string& operation) :
+m_parent(parent),
+m_cost(cost),
+m_operation(operation)
 {
 }
 
-EditDistance::BackTrace::BackTrace()
+EditDistance::Cost::Cost()
 {
 }
 
-EditDistance::BackTrace::~BackTrace()
+EditDistance::Cost::~Cost()
 {
 }
 
