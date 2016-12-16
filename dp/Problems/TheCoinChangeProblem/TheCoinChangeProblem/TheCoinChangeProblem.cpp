fc062ee9f44d48c90a8a512921bb0f798267c24c
Dragan Ostojic <dostoji@Global.Bio-Rad.com>
2016-12-16 16:17:03 -0500

    saved

--- dp/Problems/TheCoinChangeProblem/TheCoinChangeProblem/TheCoinChangeProblem.cpp
+++ dp/Problems/TheCoinChangeProblem/TheCoinChangeProblem/TheCoinChangeProblem.cpp
@@ -11,18 +11,43 @@
 // Output:
 // 4
 
+
+class TheCoinChangeProblem
+{
+public:
+	TheCoinChangeProblem(int n, const std::vector<int>& coins);
+	~TheCoinChangeProblem();
+
+private:
+	int m_n;
+	const std::vector<int> m_coins;
+};
+
+TheCoinChangeProblem::TheCoinChangeProblem(int n, const std::vector<int>& coins) :
+m_n(n),
+m_coins(coins)
+{
+}
+
+TheCoinChangeProblem::~TheCoinChangeProblem()
+{
+}
+
 int 
 main()
 {
-	int n, m;
-	std::vector<int> coins;
-	std::cin >> n >> m;
-	for (int i = 0; i < m; i++)
-	{
-		int c;
-		std::cin >> c;
-		coins.push_back(c);
-	}
+	//int n, m;
+	//std::vector<int> coins;
+	//std::cin >> n >> m;
+	//for (int i = 0; i < m; i++)
+	//{
+	//	int c;
+	//	std::cin >> c;
+	//	coins.push_back(c);
+	//}
+	int n1 = 4;
+	std::vector<int> coins1 = { 1, 2, 3 };
+	TheCoinChangeProblem theCoinChangeProblem1(n1, coins1);
 	std::cout << "Press any key to exit..." << std::endl;
 	getchar();
 	return 0;
