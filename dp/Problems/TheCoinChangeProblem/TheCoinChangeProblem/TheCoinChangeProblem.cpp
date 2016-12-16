5eb44285f3aa31681431cc85e014ed971a1468e9
Dragan Ostojic <dostoji@Global.Bio-Rad.com>
2016-12-16 16:12:21 -0500

    saved

--- dp/Problems/TheCoinChangeProblem/TheCoinChangeProblem/TheCoinChangeProblem.cpp
+++ dp/Problems/TheCoinChangeProblem/TheCoinChangeProblem/TheCoinChangeProblem.cpp
@@ -1,8 +1,30 @@
 //
+#include <cstdio>
+#include <iostream>
+#include <vector>
+
+// Example:
+// Input:
+// 4 3
+// 1 2 3
+
+// Output:
+// 4
 
 int 
-main(int argc, char* argv[])
+main()
 {
+	int n, m;
+	std::vector<int> coins;
+	std::cin >> n >> m;
+	for (int i = 0; i < m; i++)
+	{
+		int c;
+		std::cin >> c;
+		coins.push_back(c);
+	}
+	std::cout << "Press any key to exit..." << std::endl;
+	getchar();
 	return 0;
 }
 
