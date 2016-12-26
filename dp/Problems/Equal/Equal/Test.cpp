e2e4c4137057d527fa79b09d8531ffb15061a1fa
Dragan Ostojic <dragan.ostojic@outlook.com>
2016-12-26 02:01:23 -0500

    saved

--- dp/Problems/Equal/Equal/Test.cpp
+++ dp/Problems/Equal/Equal/Test.cpp
@@ -12,24 +12,28 @@ public:
 	Equal(const std::vector<int>& initialNumberOfChocolates);
 	~Equal();
 	friend std::ostream& operator<<(std::ostream& os, Equal& equal);
+	int mGetMinNumberOfSteps() const;
 
 private:
 	std::string mVectorToString(const std::vector<int>& v);
+	int mDp(const std::vector<int>& numberOfChocolates);
 
 private:
 	std::vector<int> m_initialNumberOfChocolates;
 	std::unordered_map<std::string, int> m_dp;
+	int m_minNumberOfSteps;
 };
 
 //
 
 Equal::Equal(const std::vector<int>& initialNumberOfChocolates):
-m_initialNumberOfChocolates(initialNumberOfChocolates)
+m_initialNumberOfChocolates(initialNumberOfChocolates),
+m_minNumberOfSteps(0)
 {
 	int max = *std::max_element(m_initialNumberOfChocolates.begin(), m_initialNumberOfChocolates.end());
 	std::vector<int> candidateSolution1;
 	candidateSolution1.assign(m_initialNumberOfChocolates.size(), max);
-	m_dp[mVectorToString(m_initialNumberOfChocolates)] = 0;
+	int minNumberOfSteps = mDp(candidateSolution1);
 }
 
 //
@@ -64,11 +68,30 @@ Equal::mVectorToString(const std::vector<int>& v)
 
 //
 
+int 
+Equal::mDp(const std::vector<int>& numberOfChocolates)
+{
+	//m_dp[mVectorToString(m_initialNumberOfChocolates)] = 0;
+	return 0;
+}
+
+//
+
+int 
+Equal::mGetMinNumberOfSteps() const
+{
+	return m_minNumberOfSteps;
+}
+
+//
+
 int
 main()
 {
+	std::cout << "Test 1" << std::endl;
 	Equal equal1({ 2, 2, 3, 7 });
 	std::cout << equal1 << std::endl;
+	std::cout << "min number of steps: " << equal1.mGetMinNumberOfSteps() << std::endl;
 	getchar();
 	return 0;
 }
