e5fe1eac61962d4d69ca0060ecb9ce64cc36e2bb
Dragan Ostojic <dragan.ostojic@outlook.com>
2016-12-25 22:39:01 -0500

    saved

--- dp/Problems/Equal/Equal/Test.cpp
+++ dp/Problems/Equal/Equal/Test.cpp
@@ -1,9 +1,50 @@
+#include <vector>
 #include <iostream>
 
 // 
 
+class Equal
+{
+public:
+	Equal(const std::vector<int>& numberOfChocolates);
+	~Equal();
+	void mPrintNumberOfChocolates();
+
+private:
+	std::vector<int> m_numberOfChocolates;
+};
+
+//
+
+Equal::Equal(const std::vector<int>& numberOfChocolates):
+m_numberOfChocolates(numberOfChocolates)
+{
+}
+
+//
+
+Equal::~Equal()
+{
+}
+
+//
+
+void 
+Equal::mPrintNumberOfChocolates()
+{
+	for (const auto& numberOfChoclolates : m_numberOfChocolates)
+	{
+		std::cout << numberOfChoclolates << " ";
+	}
+}
+
+//
+
 int
 main()
 {
+	Equal equal1({ 2, 2, 3, 7 });
+	equal1.mPrintNumberOfChocolates();
+	getchar();
 	return 0;
 }
