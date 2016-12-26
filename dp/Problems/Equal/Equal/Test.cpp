242f9aad503a2869ec3b55379bb50b94cfebc6d5
Dragan Ostojic <dragan.ostojic@outlook.com>
2016-12-25 23:30:28 -0500

    saved

--- dp/Problems/Equal/Equal/Test.cpp
+++ dp/Problems/Equal/Equal/Test.cpp
@@ -1,6 +1,8 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <unordered_map>
+#include <sstream>
 
 // 
 
@@ -9,10 +11,14 @@ class Equal
 public:
 	Equal(const std::vector<int>& initialNumberOfChocolates);
 	~Equal();
-	friend std::ostream& operator<<(std::ostream& os, const Equal& equal);
+	friend std::ostream& operator<<(std::ostream& os, Equal& equal);
+
+private:
+	std::string mVectorToString(const std::vector<int>& v);
 
 private:
 	std::vector<int> m_initialNumberOfChocolates;
+	std::unordered_map<std::string, int> m_dp;
 };
 
 //
@@ -23,6 +29,7 @@ m_initialNumberOfChocolates(initialNumberOfChocolates)
 	int max = *std::max_element(m_initialNumberOfChocolates.begin(), m_initialNumberOfChocolates.end());
 	std::vector<int> candidateSolution1;
 	candidateSolution1.assign(m_initialNumberOfChocolates.size(), max);
+	m_dp[mVectorToString(m_initialNumberOfChocolates)] = 0;
 }
 
 //
@@ -34,15 +41,25 @@ Equal::~Equal()
 //
 
 std::ostream& 
-operator<<(std::ostream& os, const Equal& equal)
+operator<<(std::ostream& os, Equal& equal)
 {
-	os << "[ ";
-	for (size_t i = 0; i < equal.m_initialNumberOfChocolates.size(); i++)
-	{
-		os << equal.m_initialNumberOfChocolates[i] << " ";
-	}
-	os << "]";
+	os << equal.mVectorToString(equal.m_initialNumberOfChocolates);
 	return os;
+}
+
+//
+
+std::string
+Equal::mVectorToString(const std::vector<int>& v)
+{
+	std::stringstream ss;
+	ss << "[ ";
+	for (size_t i = 0; i < v.size(); i++)
+	{
+		ss << v[i] << " ";
+	}
+	ss << "]";
+	return ss.str();
 }
 
 //
