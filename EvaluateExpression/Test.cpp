4d3ed4afa9da3856e663c72548a311210b359736
Dragan Ostojic <dragan.ostojic@outlook.com>
2017-01-19 01:54:10 -0500

    saved

--- EvaluateExpression/Test.cpp
+++ EvaluateExpression/Test.cpp
@@ -1,9 +1,78 @@
 //
 
 #include <cstdio>
+#include <iostream>
+#include <string>
 
-int main()
+//
+
+class EvaluateExpression
 {
+public:
+	EvaluateExpression(const std::string& expression);
+	int mGetResult();
+
+private:
+	std::string m_expression;
+};
+
+EvaluateExpression::EvaluateExpression(const std::string& expression) :
+m_expression(expression)
+{
+}
+
+int 
+EvaluateExpression::mGetResult()
+{
+	return 0;
+}
+
+class EvaluateExpressionTest
+{
+public:
+	EvaluateExpressionTest(const std::string& testName, const std::string& expression, int expectedResult);
+	void mAssert(int result);
+
+private:
+	std::string m_testName;
+	std::string m_expression;
+	int m_expectedResult;
+};
+
+EvaluateExpressionTest::EvaluateExpressionTest(const std::string& testName, const std::string& expression, int expectedResult) :
+m_testName(testName),
+m_expression(expression),
+m_expectedResult(expectedResult)
+{
+}
+
+void 
+EvaluateExpressionTest::mAssert(int result)
+{
+	std::cout << m_testName << std::endl;
+	std::cout << "Expression: " << m_expression << std::endl;
+	std::cout << "Expected: " << m_expectedResult << std::endl;
+	std::cout << "Actual: " << result << std::endl;
+	if (m_expectedResult == result)
+	{
+		std::cout << "PASS" << std::endl;
+	}
+	else
+	{
+		std::cout << "FAIL" << std::endl;
+	}
+}
+
+int 
+main()
+{
+	
+	std::string testName1("Test 1");
+	std::string expression1("1 + 1");
+	int result1 = 2;
+	EvaluateExpressionTest evaluateExpressionTest1(testName1, expression1, result1);
+	EvaluateExpression evaluateExpression1(expression1);
+	evaluateExpressionTest1.mAssert(evaluateExpression1.mGetResult());
 	getchar();
   return 0;
 }
